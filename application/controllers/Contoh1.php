79255ff5d545d230f7a63d65d37ccf22a9f1a069
riskisosa <riskiaditya030114@gmail.com>
2021-10-19 18:30:40 +0700

    Project Pertemuan 1-6

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Rizqy Aditya Sosa
+            saya tinggal di daerah Marunda 
+            olahraga yang saya sukai adalah 
+            voli dan berenang";
+    }
+}
---
+++ application/views/v_about.php
@@ -0,0 +1,27 @@
+<li><a href="<?php echo base_url().'index.php/web' ?>"></a></li>
+                    <li><a href="<?php echo 
+base_url().'index.php/web/about' ?>"></a></li>
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+     <li>Nama Depan : Rizqy</li>
+     <li>Nama Belakang : Aditya Sosa</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+     <li> Jalan Bahari 5 A4 No.15 Tanjung Priok Jakarta Utara</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+     <li>DKI JAKARTA</li>
+ </ul>
+ 
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+     <li>Berenang</li>
+     <li>Voli</li>
+ </ul>
+</section>
