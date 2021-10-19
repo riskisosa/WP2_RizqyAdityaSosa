173af498013816760d513173b568e557683f8cb8
riskisosa <65360244+riskisosa@users.noreply.github.com>
2021-10-19 18:33:32 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -32,7 +32,7 @@ $config['base_url'] = 'http://localhost/pustaka-booking/';
 |
 | Typically this will be your index.php file, unless you've renamed it to
 | something else. If you are using mod_rewrite to remove the page set this
-| variable so that it is blan.
+| variable so that it is blank.
 |
 */
 $config['index_page'] = '';
